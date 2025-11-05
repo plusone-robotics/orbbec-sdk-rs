39915f8d0d80a1fdcbc0e30fbed26647e60e9be0
jaredplusone <jared.perkins@plusonerobotics.com>
2025-11-05 15:45:15 -0600

    clean up function inputs

--- src/sys.rs
+++ src/sys.rs
@@ -18,9 +18,9 @@ mod orb {
 
     include!("../bindings/bindings.rs");
 }
-
+use crate::sys::enums::OBLogSeverity;
 // Allow user to set logger level
-pub fn set_logger_severity(level: orb::OBLogSeverity) {
+pub fn set_logger_severity(level: OBLogSeverity) {
 
     unsafe {
         orb::ob_set_logger_severity(level, std::ptr::null_mut());
