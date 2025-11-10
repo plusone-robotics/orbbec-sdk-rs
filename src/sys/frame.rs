5cbc344005572ff06c2d016b7157ec9cacb5e480
jaredplusone <jared.perkins@plusonerobotics.com>
2025-11-10 17:12:10 -0600

    fix duplicate code

--- src/sys/frame.rs
+++ src/sys/frame.rs
@@ -177,15 +177,4 @@ impl OBFrame {
 
         Ok(index)
     }
-
-    /// Get the frame hardware timestamp in microseconds
-    pub fn get_timestamp_us(&self) -> Result<u64, OBError> {
-        let mut err_ptr = std::ptr::null_mut();
-
-        let timestamp = unsafe { orb::ob_frame_get_timestamp_us(self.inner, &mut err_ptr) };
-
-        OBError::consume(err_ptr)?;
-
-        Ok(timestamp)
-    }
 }
